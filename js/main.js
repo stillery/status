fe8e08862354f57260bbb84ff83b53fca9022386
Alfred Xing <alfredxing@users.noreply.github.com>
2015-12-26 20:17:40 -0800

    Add expoential markers

--- js/main.js
+++ js/main.js
@@ -8,14 +8,31 @@ function get(callback) {
 }
 
 function display(data) {
+    data = data.reverse();
     console.log(data);
 
-    var last = data[data.length - 1];
+    var last = data[0];
     $('#status').innerHTML = last.up ? 'Everything\'s good!' : 'Uh oh, something\'s gone wrong.';
 
-    var dots = '';
-    for (var i = data.length; i > 0; i--) {
-        dots += '<div class="dot" data-up="' + data[i - 1].up.toString() + '"></div>';
+    var dots = '<div class="marker">Now</div>';
+    for (var i = 0; i < data.length; i++) {
+        if (i > 36) {
+            var exp = Math.log(i / 36) / Math.log(2);
+            if (Math.floor(exp) === exp) {
+                var hours = i / 12;
+                var text;
+                if (hours < 24) {
+                    text = hours + ' hours ago';
+                } else if (hours === 24) {
+                    text = 'A day ago';
+                } else {
+                    text = hours / 24 + ' days ago';
+                }
+                dots += '<div class="marker">' + text + '</div>';
+            }
+        }
+
+        dots += '<div class="dot" data-up="' + data[i].up.toString() + '"></div>';
     }
     $('#dots').innerHTML = dots;
 }
